7aeab2ae5340ee1d17c69f7ac1a3df2d63872559
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-06-11 16:40:00 -0700

    add home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Form from './components/Form'
-
+import Form from "./Components/Form";
+import Home from "./Components/Home/Home";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path='/' component={Form} />
+          <Route exact path="/" component={Form} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </div>
     </BrowserRouter>
-    
   );
 }
 
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <button className="btn waves-effect">Homeless</button>
+        <button className="btn waves-effect">Providers</button>
+      </div>
+    );
+  }
+}
+
+export default Home;
