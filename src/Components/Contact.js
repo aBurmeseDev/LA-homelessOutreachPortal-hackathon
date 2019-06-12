698acf26fce6f03377dbef8897436153d97fc392
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-06-12 14:57:08 -0700

    add contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,34 +1,104 @@
 import React, { Component } from "react";
+import M from "materialize-css";
 
 
 class Contact extends Component {
 
     componentDidMount() {
+        document.addEventListener("DOMContentLoaded", function() {
+            const elem1 = document.querySelectorAll("select");
+            const instance1 = M.FormSelect.init(elem1);
+            const elem2 = document.querySelectorAll(".characterCounter");
+            const instance2 = M.CharacterCounter.init(elem2);
+        });
 
     }    
 
     render() {
         return (
-            <div className="row">
-                <form className="col s12">
-                    <div>Hello World</div>
-                    {/* <div className="input-field col s12">
-                        <select>
-                            <option value="" disabled selected>What can we help you with?</option>
-                            <option value="1">Option 1</option>
-                            <option value="2">Option 2</option>
-                            <option value="3">Option 3</option>
-                            <option value="4">Option 4</option>
-                        </select>
-                    </div> */}
-                    {/* <div className="row">
-                        <div className="input-field col s6">
-                            <input type="text" id="firstName" placeholder="First Name" className="validate">
-                                <label htmlFor="textarea1">First Name</label>
-                            </input>
+            <div className="container">
+                <div className="row">
+                <h4>Contact Us</h4>
+                        <h5>We want to hear from you</h5>
+                        <div className="divider"></div><br/>
+                <form className="col l8 s12">
+                      
+                        <div className="row">
+                        <div className="input-field col s12">
+                            <select>
+                                <option value="" disabled selected>What can we help you with?</option>
+                                <option value="1">Dashboards</option>
+                                <option value="2">Homeless Management Information System (HMIS)</option>
+                                <option value="3">Grievances</option>
+                                <option value="4">Homeless Count</option>
+                                <option value="5">Homeless Services</option>
+                                <option value="6">LA-HOP</option>
+                                <option value="7">Notices of Funding Availability (NOFAs)</option>
+                                <option value="8">Outcomes/Reporting</option>
+                                <option value="9">Procurement Opportunities</option>
+                                <option value="10">Technical Web Support</option>
+                            </select>
+                            <label>Select From The Dropdown Menu</label>
                         </div>
-                    </div> */}
-                </form>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input 
+                                    type="text" 
+                                    id="firstName" 
+                                    className="validate" 
+                                    />
+                                    <label for="firstName">First Name</label>
+                            </div>
+                            <div className="input-field col s6">
+                                <input 
+                                    type="text" 
+                                    id="lasttName" 
+                                    className="validate" 
+                                    />
+                                    <label for="lastName">Last Name</label>
+                            </div>
+                            <div className="input-field col s10">
+                                <input 
+                                    type="email" 
+                                    id="email" 
+                                    className="validate" 
+                                    />
+                                    <label for="email">Email</label>
+                                    <span class="helper-text" data-error="Incomplete email address" >We will never use your email for marketing purposes.</span>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div class="input-field col s10">
+                                <textarea
+                                id="textarea1"
+                                placeholder="Please tell us about your issue"
+                                className="materialize-textarea characterCounter"
+                                data-length="120"
+                                />
+                                <label for="textarea1">Your Message</label>
+                            </div>
+                        </div>
+                        <button className="btn red lighten-1 ">Submit</button>
+                    </form>
+                    <div className="col l4">
+                        <h5 style={{textAlign: "left"}}>Contact Details</h5>
+                        <p>Question, comment or concern? Our contact form is the best way to get in touch with someone at LAHSA.
+
+</p>
+<h6 style={{textAlign: "left"}}>Address</h6>
+<p  style={{textAlign: "left"}}>811 Wilshire Blvd, 
+6th Floor, 
+Los Angeles, CA 90017</p>
+<p>
+Tel: (213) 683-3333
+<br/>
+Tel: (213) 553-8488
+<br/>
+Tel: (213) 553-8489
+</p>
+                    </div>
+                </div>
             </div>
         )
     }
