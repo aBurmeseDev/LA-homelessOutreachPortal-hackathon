2eb77d4be43e473624f2871d7e9bf9312b8b24ba
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-06-13 14:48:23 -0700

    completes providerLocation form

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,101 +1,105 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper,Marker } from 'google-maps-react';
-import Geocode from 'react-geocode'
-import SearchBar from '../Components/SearchBar'
-import { GoogleComponent } from 'react-google-location' 
-import Autocomplete from 'react-google-autocomplete';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import Geocode from "react-geocode";
+import SearchBar from "../Components/SearchBar";
+import { GoogleComponent } from "react-google-location";
+import Autocomplete from "react-google-autocomplete";
 
 Geocode.setApiKey("AIzaSyCh-X4wpp1dAcIoEZiLhOHkASJwlwnCWg4");
 Geocode.enableDebug();
 
-const API_KEY = "AIzaSyDvWu69XjuxqaUQnd8ZWDl7QEtEQUwNRFY"
-const mapStyles = {
-    top:"5em",
-    width: '60%',
-    height: '90%',
-    place:null,
-    position:"absolute"
-    
-  };
-const boxStyle ={
-    position:"relative"
-}
-const searchBoxStyle ={
-    position:"absolute",
-    zIndex: "3",
-    backgroundColor: "white",
-    left: "20em",
-    width: "30em",
-    top: "10em",
-    left: "20em"
-}
+const API_KEY = "AIzaSyDvWu69XjuxqaUQnd8ZWDl7QEtEQUwNRFY";
+const mapStyle = {
+  width: "100%",
+  height: "20rem",
+  place: null
+};
 
+// const searchBoxStyle ={
+//     position:"absolute",
+//     zIndex: "3",
+//     backgroundColor: "white",
+//     left: "20em",
+//     width: "30em",
+//     top: "10em",
+//     left: "20em"
+// }
 
 class MapComponent extends Component {
-    state={
-        latitude: null,
-        longitude: null,
-        isMarkerShown: false,
-        query: '',
-        place:null,
-        address:null
+  state = {
+    latitude: null,
+    longitude: null,
+    isMarkerShown: false,
+    query: "",
+    place: null,
+    address: null
+  };
 
-    }
-  
-    componentDidMount() {
-        navigator.geolocation.getCurrentPosition(
-            (position) =>{  this.setState({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                lacated: true
-              });
-            }
-        );
-        
-    }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        lacated: true
+      });
+    });
+  }
 
-    componentWillUpdate(){
-        Geocode.fromLatLng(this.state.latitude, this.state.longitude).then(
-            response => {
-              console.log(response.results[0].formatted_address);
-              this.setState({address:response.results[0].formatted_address})
-              this.props.updateAddress(this.state.address)
-            },
-            error => {
-              console.error(error);
-            }
-          );
-    }
-  
-    filterMarkers(e){
-        e.preventDefault()
-        console.log(e.target)
-    }
+  componentWillUpdate() {
+    Geocode.fromLatLng(this.state.latitude, this.state.longitude).then(
+      response => {
+        console.log(response.results[0].formatted_address);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                <div className="container">
-                    <div style={searchBoxStyle}>
-                        <GoogleComponent apiKey={API_KEY}  language={'en'} country={'country:us'} coordinates={true} onChange={(e) => { this.setState({ latitude: e.coordinates.lat, longitude: e.coordinates.lng }) }} />  
-                    </div>
+  filterMarkers(e) {
+    e.preventDefault();
+    console.log(e.target);
+  }
 
-                    <Map google={this.props.google} zoom={14} style={mapStyles}  center={{lat:this.state.latitude,lng:this.state.longitude}} >
-                        <Marker title={"Your Location"} name={'SOMA'}  position={{lat: this.state.latitude, lng: this.state.longitude}} />
-                    </Map>
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div style={{ marginTop: "3rem" }}>
+            <label>ENTER ADDRESS OF PERSON/PEOPLE IN NEED</label>
+            <GoogleComponent
+              apiKey={API_KEY}
+              language={"en"}
+              country={"country:us"}
+              coordinates={true}
+              onChange={e => {
+                this.setState({
+                  latitude: e.coordinates.lat,
+                  longitude: e.coordinates.lng
+                });
+              }}
+            />
+          </div>
 
-
-                </div>
-
-                </div>
-                </div>
-                
-
-            
-
-        );
-    }
+          <Map
+            google={this.props.google}
+            zoom={14}
+            center={{ lat: this.state.latitude, lng: this.state.longitude }}
+            style={mapStyle}
+          >
+            <Marker
+              title={"Your Location"}
+              name={"SOMA"}
+              position={{
+                lat: this.state.latitude,
+                lng: this.state.longitude
+              }}
+            />
+          </Map>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
--- src/Components/ProviderLocation.js
+++ src/Components/ProviderLocation.js
@@ -1,27 +1,114 @@
 import React, { Component } from "react";
-import Map from '../Components/Map'
+import Map from "../Components/Map";
+import M from "materialize-css";
 
 class ProviderLocation extends Component {
-    state={
-        address:null
-    }
-    
-    continue = e =>{
-        e.preventDefault();
-        this.props.continue();
-    }
- 
-    render() {
-        return (
-            <div>
-                <Map />
-                <button class="btn waves-effect black" onClick={this.continue} type="submit" id="location" name="action">Continue
-                    <i class="material-icons right">send</i>
-                </button>  
+  state = {
+    address: null
+  };
+  componentDidMount() {
+    document.addEventListener("DOMContentLoaded", function() {
+      const elems = document.querySelectorAll(".datepicker");
+      const instances = M.Datepicker.init(elems);
+      const elem2 = document.querySelectorAll(".characterCounter");
+      const instance2 = M.CharacterCounter.init(elem2);
+    });
+  }
+
+  continue = e => {
+    e.preventDefault();
+    this.props.continue();
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="row" style={{ position: "relative" }}>
+          <Map />
+        </div>
+
+        <div className="row" style={{ marginTop: "20rem" }}>
+          <div className="row">
+            <div className="col s6 l6">
+              <label for="datepicker">DATE LAST SEEN</label>
+              <input type="text" class="datepicker" />
             </div>
-            
-        )
-    }
+            <div className="col s6">
+              <label>SEEN BEFORE?</label>
+              <form action="#" style={{ display: "flex" }}>
+                <p>
+                  <label>
+                    <input name="group1" type="radio" />
+                    <span style={{ paddingRight: "10px" }}>YES </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name="group1" type="radio" />
+                    <span>NO </span>
+                  </label>
+                </p>
+              </form>
+            </div>
+            <div className="col s12">
+              {" "}
+              <label>SEEN BEFORE?</label>
+              <form action="#" style={{ display: "flex" }}>
+                <p>
+                  <label>
+                    <input name="group1" type="radio" />
+                    <span style={{ paddingRight: "6px" }}>STREET </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name="group1" type="radio" />
+                    <span style={{ paddingRight: "6px" }}>TENT </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name="group1" type="radio" />
+                    <span style={{ paddingRight: "6px" }}>VEHICLE </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name="group1" type="radio" />
+                    <span style={{ paddingRight: "6px" }}>OTHER </span>
+                  </label>
+                </p>
+              </form>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s12" />
+          </div>
+          <div className="row">
+            <div className="col s12">
+              <label for="textarea">DESCRIPTION OF LOCATION</label>
+              <textarea
+                id="textarea1"
+                className="materialize-textarea characterCounter"
+                data-length="120"
+              />
+            </div>
+          </div>
+          <button
+            className="btn waves-effect black right"
+            onClick={this.continue}
+            type="submit"
+            id="location"
+            name="action"
+            style={{ marginTop: "2rem" }}
+          >
+            Continue
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProviderLocation;
